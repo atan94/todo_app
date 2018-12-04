68b33a13a0c9a88f6845315b0484d4b91a5ecba6
atan94 <amostan94@gmail.com>
2018-12-04 22:07:50 +0800

    Updates styles of homepage

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { Avatar } from 'react-native-elements';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Button 
+          title={'Hello'} 
+          color='green' 
+          onPress={() => { console.log('hello'); }}
+        />
+        <Avatar
+  xlarge
+  rounded
+  source={{ uri: 'http://ukcdn.ar-cdn.com/recipes/land500/3f02d99c-df8f-40a4-80ae-f768b0f23dc0.jpg' }}
+  onPress={() => console.log('Works!')}
+  activeOpacity={0.7}
+        />
+        <Text style={styles.bigblue}>wassup</Text>
+        <Button 
+          title={'BYE'} 
+          color='red' 
+          onPress={() => { console.log('bye'); }}
+        />
       </View>
     );
   }
@@ -14,8 +32,13 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'yellow',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  bigblue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+  }
 });
